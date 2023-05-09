790a5630d8fd41d7ae9b68a5d1611cd2cce94e35
ravi mishra <ravimishra1017.rm@gmail.com>
2023-05-09 17:21:54 +0530

    added cart data and recyclerview

---
+++ app/src/main/java/com/example/test/CartData.kt
@@ -0,0 +1,10 @@
+package com.example.test
+
+data class CartData(
+    val imageUrl: String,
+    val buttonText: String,
+    val productAvailability: String,
+    val price: Float,
+    val title: String,
+    val rating: Float
+)
--- app/src/main/java/com/example/test/ItemAdapter.kt
+++ app/src/main/java/com/example/test/ItemAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.test
 
-class ItemAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.test.databinding.ItemRowBinding
+
+class ItemAdapter(private val mList: List<CartData>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemBinding = ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(itemBinding)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       holder.bind(mList[position])
+    }
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+    class ViewHolder(private val itemBinding: ItemRowBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(cartData: CartData) {
+            itemBinding.apply {
+                tvAvailability.text = cartData.productAvailability
+                tvPrice.text = cartData.price.toString()
+                tvProductTitle.text = cartData.price.toString()
+                Glide.with(itemView.context)
+                    .load("https://picsum.photos/seed/picsum/200/300")
+                    .centerCrop()
+                    .into(ivCart)
+            }
+        }
+    }
 }
